053a9a5d65b0a4f17de47cda50ba692776c19aeb
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-07-22 00:50:11 +0900

    update: gave it a little tweak to app.js to reference views directory correctly

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 const app = express()
+app.set('views', 'src/views')
+app.set('view engine', 'pug')
 
-app.get('/views', (req, res) => {
+app.use(((req, res, next) => {
   res.render('index', { 'title': 'css animations' })
-})
+}))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('listening on port 3000!'))
