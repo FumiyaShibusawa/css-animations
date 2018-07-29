2733d4f932b90e2f4aabd324e80ced0d9ffa5135
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-07-29 15:09:41 +0900

    update: put all the routing modules into array as argument of app.use

--- src/lib/javascripts/application.js
+++ src/lib/javascripts/application.js
@@ -4,11 +4,13 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _routes = require('../routes');
+
+var _routes2 = _interopRequireDefault(_routes);
+
 var _myGrandfathersClock = require('../routes/my-grandfathers-clock');
 
-var myGrandfathersClock = _interopRequireWildcard(_myGrandfathersClock);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+var _myGrandfathersClock2 = _interopRequireDefault(_myGrandfathersClock);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -17,11 +19,7 @@ app.use(_express2.default.static(__dirname));
 app.set('views', 'src/views');
 app.set('view engine', 'pug');
 
-app.get('/', function (req, res) {
-  res.render('index', { 'title': 'css animations' });
-});
-
-app.use('/my-grandfathers-clock', myGrandfathersClock);
+app.use([_routes2.default, _myGrandfathersClock2.default]);
 
 app.listen(4000, function () {
   return console.log('listening on port 4000!', __dirname);
--- src/public/javascripts/application.js
+++ src/public/javascripts/application.js
@@ -4,12 +4,9 @@ app.use(express.static(__dirname))
 app.set('views', 'src/views')
 app.set('view engine', 'pug')
 
-import * as myGrandfathersClock from '../routes/my-grandfathers-clock'
+import routes from '../routes'
+import myGrandfathersClock from '../routes/my-grandfathers-clock'
 
-app.get('/', (req, res) => {
-  res.render('index', { 'title': 'css animations' })
-})
-
-app.use('/my-grandfathers-clock', myGrandfathersClock);
+app.use([routes, myGrandfathersClock]);
 
 app.listen(4000, () => console.log('listening on port 4000!', __dirname))
